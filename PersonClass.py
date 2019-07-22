59b639bd8bc449366149a6e7ce4e2d99b4162020
Riz <goyalrizul1997@gmail.com>
2019-07-22 22:15:55 +0530

    implemented class in Python

---
+++ PersonClass.py
@@ -0,0 +1,36 @@
+# Learning to model a real world object in Python using class
+
+class Person:
+    # When you create an instance of person, 
+    # __init__ assigns the values to the properties of the class
+    def __init__(self,name,age,address):
+        self.name = name
+        self.age = age
+        self.address = address
+
+    # Note: When we want to access the properties of the class, 
+    # we use self.property name for ex- self.name & self.age
+    
+    # person can give intro
+    def intro(self):
+        print(f"I am {self.name}. I am {self.age} years old")
+
+    # person can walk with the given speed
+    def walk(self,speed):
+        print(f"My walking speed is {speed} km/h")
+    
+    # person can play for given hours    
+    def play(self,hours):
+        print(f"I play for {hours} hours")
+
+    # person can have given dish for lunch
+    def lunch(self,dish):
+        print(f"I ate {dish} in lunch")
+
+rizul = Person("Rizul",21,"noida")
+rizul.intro()
+rizul.walk(10)
+rizul.play(3)
+rizul.lunch("roti")
+
+
